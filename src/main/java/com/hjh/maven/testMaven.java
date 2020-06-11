0612f75e426583bfe1b17dcd430ed1d588b28f89
hejiahao298 <2760089034@qq.com>
2020-06-11 14:28:03 +0800

    分支添加

--- src/main/java/com/hjh/maven/testMaven.java
+++ src/main/java/com/hjh/maven/testMaven.java
@@ -4,6 +4,7 @@ public class testMaven {
       public String Hello(String name) {
     	  System.out.println("更新1");
     	  System.out.println("创建分支1");
+    	  System.out.println("分支添加1");
 		return "Hello"+name+"!";
 	}
 }
