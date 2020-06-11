9c7113bbbe02df3a4e76f31544a257c766ca99f6
hejiahao298 <2760089034@qq.com>
2020-06-11 14:25:02 +0800

    创建分支

--- src/main/java/com/hjh/maven/testMaven.java
+++ src/main/java/com/hjh/maven/testMaven.java
@@ -3,6 +3,7 @@ package com.hjh.maven;
 public class testMaven {
       public String Hello(String name) {
     	  System.out.println("更新1");
+    	  System.out.println("创建分支1");
 		return "Hello"+name+"!";
 	}
 }
