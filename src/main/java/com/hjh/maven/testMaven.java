7d67da4652c4c8e10599d883fb57ad7ad2783c1c
hejiahao298 <2760089034@qq.com>
2020-06-11 14:27:25 +0800

    主线添加

--- src/main/java/com/hjh/maven/testMaven.java
+++ src/main/java/com/hjh/maven/testMaven.java
@@ -4,6 +4,7 @@ public class testMaven {
       public String Hello(String name) {
     	  System.out.println("更新1");
     	  System.out.println("创建分支1");
+    	  System.out.println("主线添加");
 		return "Hello"+name+"!";
 	}
 }
